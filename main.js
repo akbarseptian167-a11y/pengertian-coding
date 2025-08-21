635c373ff2ed1e066db25b9401821d078ac8cfa6
akbarseptian167-a11y <akbarseptian167@gmail.com>
2025-08-20 19:35:15 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+//  let nama = prompt('siapa nama kamu')
+//  let usia = prompt('berapa usia kamu')
+
+//  if (usia == null){
+//      usia = '1-14'
+//  } else {
+//      usia = '15-20'
+//  }
+
+//   switch(usia) {
+//    case '1-14':
+//         alert('Kamu masih anak-anak')
+//         break
+//        case '15-20':
+//          alert('Kamu sudah remaja')
+//          break
+//      case '21-30':
+//         alert('kamu sudah dewasa')
+//         break
+//     }
+
+// // alert('nama kamu adalah ' + nama + ' dan usia kamu adalah ' + usia)
